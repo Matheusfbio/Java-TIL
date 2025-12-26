818de7edd65693164d1e53b8b97073f7bd40c860
Matheus Fábio <matheusfabiors@gmail.com>
2025-12-26 01:36:21 -0300

    Primitive types: declaration and memorie

---
+++ src/primitivos/PrimitiveTypes.java
@@ -0,0 +1,9 @@
+package primitivos;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        System.out.println(age);
+    }
+}
