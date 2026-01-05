69d0c470626d9399426cd3b992be5e98c6ca2b08
Matheus Fábio <matheusfabiors@gmail.com>
2026-01-05 01:06:34 -0300

    primitive and operators types

---
+++ src/Aritimeticos/operadoresAratimeticos.java
@@ -0,0 +1,18 @@
+public class operadoresAratimeticos {
+  public static void main(String[] args) {
+    // Operadores Aritméticos: +, -, *, /, %
+    double nunber1 = 10;
+    double nunber2 = 3;
+    double sum = nunber1 + nunber2;
+    double substract = nunber1 - nunber2;
+    double multiply = nunber1 * nunber2;
+    double divide = nunber1 / nunber2;
+    double module = nunber1 % nunber2;
+
+    System.out.println("Sum is: " + sum);
+    System.out.println("Substract is: " + substract);
+    System.out.println("Multiply is: " + multiply);
+    System.out.println("Divide is: " + divide);
+    System.out.println("Module is: " + module);
+  }
+}
--- src/primitivos/PrimitiveTypes.java
+++ src/primitivos/PrimitiveTypes.java
@@ -2,8 +2,12 @@ package primitivos;
 
 public class PrimitiveTypes {
     public static void main(String[] args) {
-        //int, double, float, char, byte, short, long, boolean
+        //int, double, float, char, byte, short, git long, boolean
         int age = 10;
+        float salary = (float) 2000.0D;
+        char character = 'M'; // only character for time
         System.out.println(age);
+        System.out.println(salary);
+        System.out.println("char "+character);
     }
 }
